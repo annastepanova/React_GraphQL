678e6bcdb7582888a1cc5cf8bb0c252abf34d7bf
annastepanova <annastepanova1811@gmail.com>
2020-07-05 14:36:04 -0400

    implementing routes authentication

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Users from './Users'
+import { BrowserRouter as Router } from 'react-router-dom'
 import MainNavigation from './NavComponents/MainNavigation'
-import { AuthProvider } from './context/Auth'
+import { AuthProvider} from './context/Auth'
+import AuthRoutes from './AuthRoute'
 
 import './App.css'
 
 const App = () => {
-
-  let routes = (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/users" component={Users} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Redirect to="/" />
-      </Switch>
-  )
 
 
   return (
     <AuthProvider>
       <Router>
         <MainNavigation />
-        <main id="center">
-          {routes}
-        </main>
+         <AuthRoutes/>
       </Router>
     </AuthProvider> 
   )
---
+++ src/AuthRoutes.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Users from './Users'
+import { AuthContext } from './context/Auth'
+
+const AuthRoutes = () => {
+
+  const { user } = useContext(AuthContext)
+
+  let routes
+
+  if (user) {
+    routes = (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/users" component={Users} />
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
+  else {
+    routes = (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Redirect to="/signup" />
+      </Switch>
+    )
+  }
+
+  return (
+
+    <main id="center">
+      {routes}
+    </main>
+
+  )
+}
+
+export default AuthRoutes
